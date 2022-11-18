a905266c9355988e611cd72457ff5d4d24150490
WenhaoCao0729 <77565060+WenhaoCao0729@users.noreply.github.com>
2022-11-18 14:29:07 -0700

    Add files via upload

--- client.ts
+++ client.ts
@@ -15,7 +15,9 @@ class GameScene extends Phaser.Scene {
   private wsClient?: WebSocket;
   private sprite?: Phaser.GameObjects.Sprite;
 
-  constructor() { super({ key: "GameScene" }); }
+  constructor() {
+    super({ key: "GameScene" });
+  }
 
   /**
    * Load the assets required by the scene
@@ -28,6 +30,33 @@ class GameScene extends Phaser.Scene {
    * Instantiate the private variables required by the scene
    */
   public init() {
+    function uuid(
+      a?: any // placeholder
+    ): string {
+      return a // if the placeholder was passed, return
+        ? // a random number from 0 to 15
+          (
+            a ^ // unless b is 8,
+            ((Math.random() * // in which case
+              16) >> // a random number from
+              (a / 4))
+          ) // 8 to 11
+            .toString(16) // in hexadecimal
+        : // or otherwise a concatenated string:
+          (
+            (1e7).toString() + // 10000000 +
+            -1e3 + // -1000 +
+            -4e3 + // -4000 +
+            -8e3 + // -80000000 +
+            -1e11
+          ) // -100000000000,
+            .replace(
+              // replacing
+              /[018]/g, // zeroes, ones, and eights with
+              uuid // random hex digits
+            );
+    }
+
     // Initialize the websocket client
     this.wsClient = new WebSocket(`ws://${this.HOST}:${this.PORT}`);
     this.wsClient.onopen = (event) => {
@@ -35,34 +64,37 @@ class GameScene extends Phaser.Scene {
       console.log(event);
 
       // Start of the drag event (mouse click down)
-      this.input.on("dragstart", (
-        _: Phaser.Input.Pointer,
-        gObject: Phaser.GameObjects.Sprite
-      ) => {
-        gObject.setTint(0xff0000);
-      });
+      this.input.on(
+        "dragstart",
+        (_: Phaser.Input.Pointer, gObject: Phaser.GameObjects.Sprite) => {
+          gObject.setTint(0xff0000);
+        }
+      );
 
       // During the drag event (mouse movement)
-      this.input.on("drag", (
-        _: Phaser.Input.Pointer,
-        gObject: Phaser.GameObjects.Sprite,
-        dragX: number,
-        dragY: number
-      ) => {
-        gObject.x = dragX;
-        gObject.y = dragY;
-        this.wsClient!.send(JSON.stringify({ x: gObject.x, y: gObject.y }));
-      });
+      this.input.on(
+        "drag",
+        (
+          _: Phaser.Input.Pointer,
+          gObject: Phaser.GameObjects.Sprite,
+          dragX: number,
+          dragY: number
+        ) => {
+          gObject.x = dragX;
+          gObject.y = dragY;
+          this.wsClient!.send(JSON.stringify({ x: gObject.x, y: gObject.y }));
+        }
+      );
 
       // End of the drag event (mouse click up)
-      this.input.on("dragend", (
-        _: Phaser.Input.Pointer,
-        gObject: Phaser.GameObjects.Sprite
-      ) => {
-        gObject.clearTint();
-        this.wsClient!.send(JSON.stringify({ x: gObject.x, y: gObject.y }));
-      });
-    }
+      this.input.on(
+        "dragend",
+        (_: Phaser.Input.Pointer, gObject: Phaser.GameObjects.Sprite) => {
+          gObject.clearTint();
+          this.wsClient!.send(JSON.stringify({ x: gObject.x, y: gObject.y }));
+        }
+      );
+    };
 
     this.wsClient.onmessage = (wsMsgEvent) => {
       console.log(wsMsgEvent);
@@ -73,7 +105,7 @@ class GameScene extends Phaser.Scene {
         this.sprite.x = actorCoordinates.x;
         this.sprite.y = actorCoordinates.y;
       }
-    }
+    };
   }
 
   /**
@@ -87,13 +119,12 @@ class GameScene extends Phaser.Scene {
   }
 }
 
-
 // Phaser configuration variables
 const config: GameConfig = {
   type: Phaser.AUTO,
   width: 800,
   height: 500,
-  scene: [GameScene]
+  scene: [GameScene],
 };
 
 class LabDemoGame extends Phaser.Game {
@@ -104,4 +135,4 @@ class LabDemoGame extends Phaser.Game {
 
 window.addEventListener("load", () => {
   new LabDemoGame(config);
-})
+});
